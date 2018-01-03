6f06254c2be534948d5ceaa5fb9fceefa8afd967
Mikhail Smolin <mikhail.smolin@exxeta.com>
2018-01-03 16:46:26 +0100

    ADD(s02u10) variable mutation and type coercion

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,4 +1,5 @@
-const name = 'John';
+// Lecture: Variables
+/* const name = 'John';
 console.log(name);
 
 const lastName = 'Smith';
@@ -8,4 +9,30 @@ const age = 26;
 console.log(age);
 
 const fullAge = true;
-console.log(fullAge);
+console.log(fullAge); */
+
+// Lecture: Variables 2
+let name = 'John';
+let age = 32;
+
+//console.log(name + age);
+//console.log(age + age);
+
+let job, isMarried;
+
+//console.log(job);
+
+job = 'teacher';
+isMarried = true;
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+//let lastName = prompt('What is the last name?');
+//console.log(lastName);
+
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
