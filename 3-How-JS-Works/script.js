5c4a0f3e1ad775d99aa60d16b353140934f38a6b
Mikhail Smolin <mikhail.smolin@exxeta.com>
2018-01-05 00:41:25 +0100

    ADD(s03u31) scoping and the scope chain

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -27,3 +27,48 @@ function foo() {
 foo();
 console.log(age);
 */
+
+
+
+
+// LECTURE: Scoping
+
+// First scoping example
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+*/
+
+
+// Example to show the difference between execution stack and scope chain
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    console.log(a + b + c + d);
+}
+*/
