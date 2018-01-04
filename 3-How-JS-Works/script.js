ef7f8b14951b88b83fbd426179a44f66f66362f1
Mikhail Smolin <mikhail.smolin@exxeta.com>
2018-01-05 00:20:14 +0100

    ADD(s03u30) hoisting in practice

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -1,77 +1,29 @@
-///////////////////////////////////////
-// Lecture: Hoisting
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////////////////////////////////////
-// Lecture: Scoping
-
-
-// First scoping example
+// LECTURE: Hoisting
 
 /*
-var a = 'Hello!';
-first();
+// functions
+calculateAge(1965);
 
-function first() {
-    var b = 'Hi!';
-    second();
-
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
+function calculateAge(year) {
+    console.log(2016 - year);
 }
+
+// retirement(1956);
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
+
+
+// variables
+
+console.log(age);
+var age = 23;
+
+function foo() {
+    console.log(age);
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 */
-
-
-
-// Example to show the differece between execution stack and scope chain
-
-/*
-var a = 'Hello!';
-first();
-
-function first() {
-    var b = 'Hi!';
-    second();
-
-    function second() {
-        var c = 'Hey!';
-        third()
-    }
-}
-
-function third() {
-    var d = 'John';
-    console.log(a + b + c + d);
-}
-*/
-
-
-
-///////////////////////////////////////
-// Lecture: The this keyword
-
-
-
-
-
-
-
-
-
