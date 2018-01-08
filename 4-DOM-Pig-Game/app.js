596db34894e88b594f6ff18349ee9a8e9c979299
Mikhail Smolin <mikhail.smolin@exxeta.com>
2018-01-08 17:44:22 +0100

    ADD(s04u39) hide dice on initial game opening

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -2,16 +2,18 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- In each turn, a player rolls a dice as many times as he whishes.
+Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost.
+After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score
+gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
-
 */
 
 const scores = [0, 0];
 const roundScores = 0;
-const activePlayer = 1;
+const activePlayer = 0;
 
 // random rumber
 const dice = Math.floor(Math.random() * 6) + 1;
@@ -19,5 +21,7 @@ const dice = Math.floor(Math.random() * 6) + 1;
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 
 // display the results
-document.querySelector(`#current-${activePlayer}`)
-    .innerHTML = `<em>${dice}</em>`;
+document.getElementById('current-' + activePlayer).innerHTML = dice;
+
+// hide dice on initial game opening
+document.querySelector('.dice').style.display = 'none';
