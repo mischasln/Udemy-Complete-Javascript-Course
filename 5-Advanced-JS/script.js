883348143fd47689e5513fd76f735da2afcfe776
Mikhail Smolin <mikhail.smolin@exxeta.com>
2018-02-23 16:39:56 +0100

    ADD(s05u55) primitives vs objects

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -29,6 +29,7 @@ console.log(mark.lastName);
 
 // object.create
 
+/*
 let personProto = {
     calculateAge: function () {
         console.log(2018 - this.yearOfBirth);
@@ -45,3 +46,42 @@ let jane = Object.create(personProto, {
     yearOfBirth: {value: 1990},
     job: {value: 'Teacher'}
 });
+*/
+
+
+// primitives vs objects
+
+// primitives
+let a = 23;
+let b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+let obj1 = {
+    name: 'Mischa',
+    age: 32
+};
+
+// objects
+let obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// functions
+let age = 27;
+let obj = {
+    name: 'Sophia',
+    city: 'Stuttgart'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'New York';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
