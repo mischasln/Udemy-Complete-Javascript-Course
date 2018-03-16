6f9c8551249742373c7948932b52edf086998e3f
Mikhail Smolin <mikhail.smolin@exxeta.com>
2018-03-16 12:39:52 +0100

    ADD(s05u56) passing functions as arguments

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -52,6 +52,7 @@ let jane = Object.create(personProto, {
 // primitives vs objects
 
 // primitives
+/*
 let a = 23;
 let b = a;
 a = 46;
@@ -85,3 +86,41 @@ change(age, obj);
 
 console.log(age);
 console.log(obj.city);
+*/
+
+
+// passing functions as arguments
+
+let years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    let arrRes = [];
+    for (let i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+}
+
+
+let ages = arrayCalc(years, calculateAge);
+let fullAges = arrayCalc(ages, isFullAge);
+let rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(rates);
