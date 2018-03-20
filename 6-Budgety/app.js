f9f7f4a5da019655418f93c68a96d50869fab54a
Mikhail Smolin <mikhail.smolin@exxeta.com>
2018-03-20 14:22:16 +0100

    ADD(s06u72) income and expense function constructors

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -1,6 +1,28 @@
 // BUDGET CONTROLLER
 let budgetController = (function () {
 
+    let Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    let Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    let data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
 
 })();
 
