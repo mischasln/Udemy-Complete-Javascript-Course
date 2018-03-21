35bcfe06f3ea2875a2b1fe1ac18dc283c936369f
Mikhail Smolin <mikhail.smolin@exxeta.com>
2018-03-21 20:47:18 +0100

    ADD(s06u86) update percentages (budget controller)

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -5,6 +5,19 @@ let budgetController = (function () {
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
+    };
+
+    Expense.prototype.calcPercentage = function (totalIncome) {
+        if (totalIncome > 0) {
+            this.percentage = Math.round((this.value / totalIncome) * 100);
+        } else {
+            this.percentage = -1;
+        }
+    };
+
+    Expense.prototype.getPercentage = function () {
+        return this.percentage;
     };
 
     let Income = function (id, description, value) {
@@ -96,6 +109,29 @@ let budgetController = (function () {
             } else {
                 data.percentage = -1;
             }
+        },
+
+        calculatePercentages: function () {
+            /*
+            a = 20
+            b = 10
+            c = 40
+            income = 1ßß
+            a = 20 / 100 = 20%
+            b = 10 / 100 = 10%
+            c = 40 / 100 = 40%
+            */
+
+            data.allItems.exp.forEach(function (cur) {
+                cur.calcPercentage(data.totals.inc);
+            });
+        },
+
+        getPercentages: function () {
+            let allPerc = data.allItems.exp.map(function (cur) {
+                return cur.getPercentage();
+            });
+            return allPerc;
         },
 
         getBudget: function () {
@@ -241,10 +277,13 @@ let controller = (function (budgetCtrl, UICtrl) {
     let updatePercentages = function () {
 
         // 1. calculate percentages
+        budgetCtrl.calculatePercentages();
 
         // 2. read percentages from budget controller
+        let percentages = budgetCtrl.getPercentages();
 
         // 3. update the UI with the new percentages
+        console.log(percentages);
     };
 
     let ctrlAddItem = function () {
