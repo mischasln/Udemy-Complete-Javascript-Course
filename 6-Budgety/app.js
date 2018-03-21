b25a57924289d173d92d6eaac141834cf32d9aa2
Mikhail Smolin <mikhail.smolin@exxeta.com>
2018-03-21 20:05:45 +0100

    ADD(s06u82) delete item from budget controller

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -63,6 +63,24 @@ let budgetController = (function () {
             return newItem;
         },
 
+        deleteItem: function (type, id) {
+            let ids, index;
+            // id = 6
+            // data.allItems[type][id];
+            // ids = [1 2 4 6 8]
+            // index = 3
+
+            ids = data.allItems[type].map(function (current) {
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+        },
+
         calculateBudget: function () {
 
             // 1. calculate total income and expenses
@@ -247,9 +265,10 @@ let controller = (function (budgetCtrl, UICtrl) {
             // inc-1
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             // 1. delete item from data structure
+            budgetCtrl.deleteItem(type, ID);
 
             // 2. delete item from the UI
 
