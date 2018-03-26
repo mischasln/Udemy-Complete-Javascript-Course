81ff17bdb6dce6a59f761d544fdfbb46a5beaf7d
Mikhail Smolin <mikhail.smolin@exxeta.com>
2018-03-26 17:39:03 +0200

    ADD(s07u102) spread operator (es6)

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -157,6 +157,7 @@ console.log(ages6);
 
 /////////////////////////////////
 // Lecture: Arrow functions 2
+
 /*
 // ES5
 var box5 = {
@@ -194,7 +195,7 @@ const box66 = {
     clickMe: () = > {
     document.querySelector('.green').addEventListener('click', () = > {
     var str = 'This is box number ' + this.position + ' and it is ' + this.color;
-alert(str);
+    alert(str);
 });
 }
 }
@@ -282,6 +283,7 @@ console.log(retirement);
 /////////////////////////////////
 // Lecture: Arrays
 
+/*
 const boxes = document.querySelectorAll('.box');
 
 //ES5
@@ -332,3 +334,41 @@ console.log(ages[full.indexOf(true)]);
 //ES6
 console.log(ages.findIndex(cur => cur >= 18));
 console.log(ages.find(cur => cur >= 18));
+*/
+
+
+
+
+/////////////////////////////////
+// Lecture: Spread operator
+
+function addFourAges (a, b, c, d) {
+    return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
+
+//ES5
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+
+//ES6
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+const bigFamily = [...familySmith, 'Lily', ...familyMiller];
+console.log(bigFamily);
+
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = 'purple');
