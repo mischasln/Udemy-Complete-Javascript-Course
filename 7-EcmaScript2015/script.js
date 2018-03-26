47fbf09314075253da56f4670ebaf084a4fc41d9
Mikhail Smolin <mikhail.smolin@exxeta.com>
2018-03-26 21:14:48 +0200

    ADD(s07u104) default parameters (es6)

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -380,7 +380,7 @@ Array.from(all).forEach(cur => cur.style.color = 'purple');
 /////////////////////////////////
 // Lecture: Rest parameters
 
-
+/*
 //ES5
 function isFullAge5() {
     //console.log(arguments);
@@ -424,3 +424,35 @@ function isFullAge6(limit, ...years) {
 }
 
 isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
+*/
+
+
+
+
+/////////////////////////////////
+// Lecture: Default parameters
+
+// ES5
+function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
+
+    lastName === undefined ? lastName = 'Smith' : lastName = lastName;
+    nationality === undefined ? nationality = 'american' : nationality = nationality;
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.nationality = nationality;
+}
+
+
+//ES6
+function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'american') {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.nationality = nationality;
+}
+
+
+var john = new SmithPerson('John', 1990);
+var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish');
