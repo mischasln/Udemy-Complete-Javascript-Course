cb74248116e174c2b2297fa99d804fc3326e561b
Mikhail Smolin <mikhail.smolin@exxeta.com>
2018-03-26 16:58:45 +0200

    ADD(s07u100) desctructuring (es6)

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -146,7 +146,7 @@ console.log(ages6);
 
 /////////////////////////////////
 // Lecture: Arrow functions 2
-
+/*
 // ES5
 var box5 = {
     color: 'green',
@@ -185,8 +185,7 @@ const box66 = {
     document.querySelector('.green').addEventListener('click', () = > {
     var str = 'This is box number ' + this.position + ' and it is ' + this.color;
 alert(str);
-})
-;
+});
 }
 }
 box66.clickMe();
@@ -221,3 +220,43 @@ Person.prototype.myFriends6 = function (friends) {
 }
 
 new Person('Mike').myFriends6(friends);
+*/
+
+
+/////////////////////////////////
+// Lecture: Destructuring
+
+// ES5
+var john = ['John', 26];
+//var name = john[0];
+//var age = john[1];
+
+
+// ES6
+const [name, age] = ['John', 26];
+console.log(name);
+console.log(age);
+
+const obj = {
+    firstName: 'John',
+    lastName: 'Smith'
+};
+
+const {firstName, lastName} = obj;
+console.log(firstName);
+console.log(lastName);
+
+const {firstName: a, lastName: b} = obj;
+console.log(a);
+console.log(b);
+
+
+function calcAgeRetirement(year) {
+    const age = new Date().getFullYear() - year;
+    return [age, 65 - age];
+}
+
+
+const [age2, retirement] = calcAgeRetirement(1990);
+console.log(age2);
+console.log(retirement);
